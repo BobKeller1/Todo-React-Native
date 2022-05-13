4d7de431437a156c0c6ec4341db9f019d0859918
Dmitry Kryshmar <Gezerer@mail.ru>
2022-05-13 17:16:08 +0500

    feature/todo-task6 | use globalStyles

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './app/App';
 import {name as appName} from './app.json';
+import globalStyles from './utils/globalStyles';
+
+globalStyles();
 
 AppRegistry.registerComponent(appName, () => App);
