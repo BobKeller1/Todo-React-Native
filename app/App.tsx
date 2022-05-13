615bdd77337d6c2f1470c75686abe99a59762af9
Dmitry Kryshmar <Gezerer@mail.ru>
2022-05-13 17:19:20 +0500

    feature/todo-task6 | separate TodoItem from app to its own component

--- app/App.tsx
+++ app/App.tsx
@@ -1,56 +1,8 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-
-const styles = StyleSheet.create({
-  todoItem: {
-    maxWidth: 370,
-    height: 84,
-    padding: 10,
-  },
-  container: {
-    padding: 13,
-    flexDirection: "row"
-  },
-  mainContainer: {
-    flex: 0.7,
-  },
-  sideContainer: {
-    flex: 0.3,
-  },
-  title: {
-    fontFamily: 'Proxima Nova',
-    fontSize: 16,
-    lineHeight: 20,
-    textAlign: 'left',
-    textAlignVertical: 'center',
-  },
-  description: {
-    fontFamily: 'Proxima Nova',
-    fontSize: 14,
-    lineHeight: 18,
-    textAlign: 'left',
-    textAlignVertical: 'center',
-    color: 'rgba(0,0,0,0.54)',
-  },
-});
+import {SafeAreaView} from 'react-native';
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <View style={styles.todoItem}>
-        <View style={styles.container}>
-          <View style={styles.mainContainer}>
-            <Text style={styles.title}>Заголовок</Text>
-            <Text style={styles.description}>Описание</Text>
-          </View>
-          <View style={styles.sideContainer}>
-            <Text style={styles.title}>Не выполнена</Text>
-            <Text style={styles.description}>дата</Text>
-          </View>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+  return <SafeAreaView />;
 };
 
 export default App;
