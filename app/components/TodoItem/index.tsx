000a335bf8de18ed9eea32a26c7b62067183b94d
Dmitry Kryshmar <Gezerer@mail.ru>
2022-05-16 09:58:33 +0500

    feature/todo-task6 | edits

---
+++ app/components/TodoItem/index.tsx
@@ -0,0 +1,95 @@
+import React, {FC} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  todoItem: {
+    height: 84,
+    padding: 10,
+  },
+  todoItemCompleted: {
+    color: 'rgba(0,0,0,0.15)',
+  },
+  container: {
+    padding: 13,
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+  },
+  mainContainer: {
+    flexGrow: 1,
+    marginBottom: 10,
+  },
+  sideContainer: {
+    flexDirection: 'column',
+  },
+  title: {
+    fontSize: 16,
+    lineHeight: 20,
+  },
+  description: {
+    fontSize: 14,
+    lineHeight: 18,
+    color: 'rgba(0,0,0,0.54)',
+  },
+  textCompleted: {
+    textDecorationLine: 'line-through',
+    color: 'rgba(0,0,0,0.24)',
+  },
+  status: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: 'rgba(0,0,0,0.54)',
+  },
+  statusCompleted: {
+    color: 'green',
+  },
+  date: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: 'rgba(0,0,0,0.54)',
+  },
+});
+
+interface TodoItemProps {
+  title: string;
+  description: string;
+  status: boolean;
+  date: string;
+  onPressHandler: () => void;
+}
+
+const TodoItem: FC<TodoItemProps> = ({
+  title,
+  description,
+  status,
+  date,
+  onPressHandler,
+}) => {
+  const todoStatus = (
+    <Text style={[styles.status, status && styles.statusCompleted]}>
+      {status ? 'Выполнено' : 'Не выполнено'}
+    </Text>
+  );
+
+  return (
+    <View>
+      <TouchableOpacity onPress={() => onPressHandler()}>
+        <View style={styles.container}>
+          <View style={styles.mainContainer}>
+            <Text style={[status && styles.textCompleted, styles.title]}>
+              {title}
+            </Text>
+            <Text style={[styles.description, status && styles.textCompleted]}>
+              {description}
+            </Text>
+          </View>
+          <View style={styles.sideContainer}>
+            {todoStatus}
+            <Text style={styles.date}>{date}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default TodoItem;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import {AppRegistry} from 'react-native';
 import App from './app/App';
 import {name as appName} from './app.json';
-import globalStyles from './utils/globalStyles';
+import {
+  globalStylesText,
+  globalStylesTouchableOpacity,
+} from './utils/globalStyles';
 
-globalStyles();
+globalStylesText();
+globalStylesTouchableOpacity();
 
 AppRegistry.registerComponent(appName, () => App);
