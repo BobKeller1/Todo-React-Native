455a5b4f3d9ba6bbbbaf1ba10b3b7710e6f77714
Dmitry Kryshmar <Gezerer@mail.ru>
2022-05-17 14:32:27 +0500

    feature/todo-task | edits: dateFormatter

---
+++ formatters/dateFormatters.ts
@@ -0,0 +1,15 @@
+const dateFormatter = (
+  date: string,
+  isNeedToSplit?: boolean,
+  separator?: string,
+  extractedElem = 0,
+) => {
+  if (isNeedToSplit) {
+    return new Date(+date).toLocaleString('ru-RU').split(`${separator}`)[
+      extractedElem
+    ];
+  }
+  return new Date(+date).toLocaleString('ru-RU');
+};
+
+export {dateFormatter};
