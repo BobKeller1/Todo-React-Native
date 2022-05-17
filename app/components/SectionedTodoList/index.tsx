74aed8731ba1cc30bd8fa606421f52d4f5779f5a
Dmitry Kryshmar <Gezerer@mail.ru>
2022-05-17 14:34:49 +0500

    feature/todo-task | edits: TodoList & SeparatedTodoList components

---
+++ app/components/SectionedTodoList/index.tsx
@@ -0,0 +1,65 @@
+import React, {FC, useMemo} from 'react';
+import {
+  ListRenderItem,
+  SectionList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {dateFormatter} from '../../../formatters/dateFormatters';
+import {ITodoItem} from '../../App';
+import TodoItem from '../TodoItem';
+import {groupBy} from '../../../utils/arrayUtils';
+
+const styles = StyleSheet.create({
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 15,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+});
+
+interface SectionedTodoListProps {
+  data: ITodoItem[];
+  onPress: () => void;
+}
+
+const SectionedTodoList: FC<SectionedTodoListProps> = ({data, onPress}) => {
+  const renderItem: ListRenderItem<ITodoItem> = ({item}) => {
+    return <TodoItem item={item} onPress={onPress} />;
+  };
+
+  const groupedByDate = groupBy(data, item => item.date);
+
+  const groupedTodo = useMemo(
+    () =>
+      Object.keys(groupedByDate).map(key => {
+        return {
+          title: key,
+          data: groupedByDate[key],
+        };
+      }),
+    [groupedByDate],
+  );
+
+  return (
+    <View style={{flex: 1}}>
+      <SectionList
+        sections={groupedTodo}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        renderSectionHeader={({section: {title}}) => (
+          <Text style={styles.sectionHeader}>
+            {dateFormatter(title, true, ',', 0)}
+          </Text>
+        )}
+      />
+    </View>
+  );
+};
+
+export default SectionedTodoList;
---
+++ app/components/TodoList/index.tsx
@@ -0,0 +1,68 @@
+import React, {FC, useCallback, useMemo} from 'react';
+import {FlatList, ListRenderItem, StyleSheet, Text, View} from 'react-native';
+import {ITodoItem} from '../../App';
+import TodoItem from '../TodoItem';
+
+const styles = StyleSheet.create({
+  emptyElementContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  listSeparator: {
+    height: 0.5,
+    width: '100%',
+    backgroundColor: '#000',
+  },
+});
+
+interface TodoListProps {
+  data: ITodoItem[];
+  searchQuery: string;
+  onPress: () => void;
+}
+
+const TodoList: FC<TodoListProps> = ({data, searchQuery, onPress}) => {
+  const todosFiltredList = useMemo(
+    () =>
+      data.filter(item =>
+        item.title.toLowerCase().includes(searchQuery.toLowerCase()),
+      ),
+    [data, searchQuery],
+  );
+
+  const FlatListEmptyElement = useCallback(() => {
+    return (
+      <View style={styles.emptyElementContainer}>
+        <Text>Извините, ничего не найдено!</Text>
+      </View>
+    );
+  }, []);
+
+  const renderItem: ListRenderItem<ITodoItem> = ({item}) => {
+    return <TodoItem item={item} onPress={onPress} />;
+  };
+
+  const FlatListItemSeparator = useCallback(() => {
+    return <View style={styles.listSeparator} />;
+  }, []);
+
+  return (
+    <View style={{flex: 1}}>
+      <FlatList
+        data={todosFiltredList}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={FlatListItemSeparator}
+        ListFooterComponent={
+          todosFiltredList.length !== 0 ? FlatListItemSeparator : null
+        }
+        ListHeaderComponent={
+          todosFiltredList.length !== 0 ? FlatListItemSeparator : null
+        }
+        ListEmptyComponent={FlatListEmptyElement}
+      />
+    </View>
+  );
+};
+
+export default TodoList;
