865e0df777b386f21066326e0f22798972fe3c35
Dmitry Kryshmar <Gezerer@mail.ru>
2022-05-25 15:38:58 +0500

    feature/todo-task15 | fix log

--- src/store/saga/index.ts
+++ src/store/saga/index.ts
@@ -2,6 +2,5 @@ import {testSaga} from './testSaga';
 import {all, call} from 'redux-saga/effects';
 
 export default function* rootSaga() {
-  console.log('саги запущены');
   yield all([call(testSaga)]);
 }
